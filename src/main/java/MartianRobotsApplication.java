cc092092a3586d261197c86ebb3cb74013f11aa8
Caitlin Woods <caitlinwoods@gmail.com>
2022-06-09 11:03:40 +0100

    handle creation and processing of robots

--- src/main/java/MartianRobotsApplication.java
+++ src/main/java/MartianRobotsApplication.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -9,23 +10,47 @@ public class MartianRobotsApplication {
     public static void main(String[] args) {
 
         List<String> input = null;
+        List<String> output = new ArrayList<>();
         String inputFilePath = "data/sampleInput.txt";
         String outputFilePath = "data/output.txt";
 
         try {
             input = Files.readAllLines(Path.of(inputFilePath));
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Please check the input file path and try again.");
+            System.exit(0);
         }
 
-        for (String line : input) {
+        // make and process robots
+        for (int i=1; i < input.size(); i++) {
+
+            String line = input.get(i).strip();
+            switch (i % 3) {
+                case 1:
+                    // new robot
+                    System.out.println("make new Robot: " + line);
+                    break;
+                case 2:
+                    // move robot
+                    System.out.println("move Robot: " + line);
+                    break;
+                case 3:
+                    System.out.println("blank line do nothing" + line);
+                    break;
+            }
+        }
+
+
+        // end of program, print and write to file
+        for (String line : output) {
             System.out.println(line);
         }
 
         try {
             Files.write(Path.of(outputFilePath), input);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("There was an error writing the solution to disk. " +
+                    "Solution still available in the console.");
         }
     }
 }
